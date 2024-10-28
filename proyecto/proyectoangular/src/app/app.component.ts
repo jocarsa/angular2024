2ddbcf2fb9e2c929de7e1f48311f813076068b8a
jocarsa <info@josevicentecarratala.com>
2024-10-28 11:24:36 +0100

    003-cabecera

--- proyecto/proyectoangular/src/app/app.component.ts
+++ proyecto/proyectoangular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SaludoComponent } from './saludo/saludo.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SaludoComponent],
+  imports: [RouterOutlet,CabeceraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
